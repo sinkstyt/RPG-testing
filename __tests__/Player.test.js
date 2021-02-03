af132fd2f7bdfafc03321ba559206328b1a6efd4
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-03 16:59:56 -0500

    get good fail strength spec
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>
    Co-authored-by: Matt Foley <mattfoley0375@gmail.com>

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,9 +1,12 @@
 import Player from '../src/js/Player.js';
 
 describe("Player", () => {
-  
-  // beforeEach()
-  
+  // let reusableTriangle;
+
+  // beforeEach(() => {
+  //   reusableTriangle = new Triangle(5, 5, 5);
+  // });
+    
   test("should correctly assign a name passed in as a string at key 'name' ", () => {
     let newPlayer = new Player("Matthew");
     expect(newPlayer.name).toEqual("Matthew");
@@ -12,5 +15,10 @@ describe("Player", () => {
   test("should correctly assign a health key with a value of 100", () => {
     let newPlayer = new Player("Matthew");
     expect(newPlayer.health).toEqual(100);
+  });
+  
+  test("should correctly assign strength with a value of 0", () => {
+    let newPlayer = new Player("Matthew");
+    expect(newPlayer.strength).toEqual(0);
   });
 });
--- src/js/Player.js
+++ src/js/Player.js
@@ -2,5 +2,18 @@ export default class Player {
   constructor(name) {
     this.name = name;
     this.health = 100;
+    this.srength = 0;
   }
 } 
+
+
+// Strength: 20
+
+// Weapon min: 100
+
+// Weapon max: 150
+
+// result of roll: 131
+
+// Strength multiplier: .05 x strength + 1
+
