910dd46503563acf34ee058ed11439e66408155d
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-03 16:42:47 -0500

    good fail health spec for Player
    Co-authored-by: Mike Pingel <mdpingel@gmail.com>
    Co-authored-by: Matt Foley <mattfoley0375@gmail.com>

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -8,4 +8,9 @@ describe("Player", () => {
     let newPlayer = new Player("Matthew");
     expect(newPlayer.name).toEqual("Matthew");
   });
+
+  test("should correctly assign a health key with a value of 100", () => {
+    let newPlayer = new Player("Matthew");
+    expect(newPlayer.health).toEqual(100);
+  });
 });
