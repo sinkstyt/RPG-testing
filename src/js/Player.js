516bd4a17ef3f1819a62b29eb85124de032c49ea
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-04 17:39:48 -0500

    pass spec for xp key on instantiation
    Co-authored-by: Michael Pingel <mdpingel@gmail.com>

--- src/js/Player.js
+++ src/js/Player.js
@@ -2,6 +2,7 @@ export default class Player {
   constructor(name) {
     this.name = name;
     this.level = 0;
+    this.xp = 100;
     this.health = 100;
     this.strength = 0;
     this.accuracy = 0;
